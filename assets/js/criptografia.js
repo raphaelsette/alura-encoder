f88c694a06774d0182fe3e836211670b0beb7da1
Raphael <107597299+raphaelsette@users.noreply.github.com>
2024-07-21 20:51:02 -0300

    refactor: alterado forma de exibição do sweetalert

--- assets/js/criptografia.js
+++ assets/js/criptografia.js
@@ -4,11 +4,20 @@ const inputSaida = document.getElementById('input-saida');
 function criptografar() {
     const msgEntrada = inputEntrada.value.trim();
     if (!msgEntrada) {
-        Swal.fire({
-            icon: "warning",
-            title: "Oops...",
-            text: "Digite um texto para criptografar.",
-            confirmButtonColor: "#3085d6"
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        });
+        Toast.fire({
+            icon: "error",
+            title: "Digite um texto para criptografar."
         });
     } else {
         const msgCriptografada = msgEntrada
@@ -18,16 +27,40 @@ function criptografar() {
             .replace(/o/g, 'ober')
             .replace(/u/g, 'ufat');
         inputSaida.value = msgCriptografada;
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        });
+        Toast.fire({
+            icon: "success",
+            title: "O texto foi criptografado."
+        });
     }
 }
 function descriptografar() {
     const msgEntrada = inputEntrada.value.trim();
     if (!msgEntrada) {
-        Swal.fire({
-            icon: "warning",
-            title: "Oops...",
-            text: "Digite um texto para descriptografar.",
-            confirmButtonColor: "#3085d6"
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        });
+        Toast.fire({
+            icon: "error",
+            title: "Digite um texto para descriptografar."
         });
     }
     const msgDescriptografada = msgEntrada
@@ -37,14 +70,39 @@ function descriptografar() {
         .replace(/ober/g, 'o')
         .replace(/ufat/g, 'u');
     inputSaida.value = msgDescriptografada;
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+        }
+    });
+    Toast.fire({
+        icon: "success",
+        title: "O texto foi descriptografado."
+    });
 }
 function copiar() {
     const copiarInputSaida = document.getElementById('input-saida');
     copiarInputSaida.select();
     document.execCommand("copy");
-    Swal.fire({
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+        }
+    });
+    Toast.fire({
         icon: "success",
-        title: "Copiado",
-        confirmButtonColor: "#3085d6"
+        title: "O texto foi copiado."
     });
 }
