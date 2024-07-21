5427ce7eaf244e2b45eee80dd795e5bd044cd823
Raphael <107597299+raphaelsette@users.noreply.github.com>
2024-07-21 19:29:38 -0300

    refactor: melhoria na organização do código e adicionado sweetalert nos avisos

--- assets/js/criptografia.js
+++ assets/js/criptografia.js
@@ -1,16 +1,37 @@
 function criptografar() {
-    var msg = document.getElementById('entrada-texto').value;
-    let novamsg = msg.replace(/e/g, 'enter').replace(/i/g, 'imes').replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat');
-    parent.document.getElementById('saida-texto').value = novamsg;
+    const msg = document.getElementById('entrada-texto').value.trim();
+    if (!msg) {
+        Swal.fire({
+            icon: "warning",
+            title: "Oops...",
+            text: "Digite um texto para criptografar.",
+            confirmButtonColor: "#3085d6"
+        });
+    } else {
+        let novamsg = msg.replace(/e/g, 'enter').replace(/i/g, 'imes').replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat');
+        parent.document.getElementById('saida-texto').value = novamsg;
+    }
 }
-    function descriptografar() {
-    var msg = document.getElementById('entrada-texto').value;
+function descriptografar() {
+    const msg = document.getElementById('entrada-texto').value.trim();
+    if (!msg) {
+        Swal.fire({
+            icon: "warning",
+            title: "Oops...",
+            text: "Digite um texto para descriptografar.",
+            confirmButtonColor: "#3085d6"
+        });
+    }
     let novamsg = msg.replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ai/g, 'a').replace(/ober/g, 'o').replace(/ufat/g, 'u');
     parent.document.getElementById('saida-texto').value = novamsg;
 }
 function copiar() {
-    var copiarmsg = document.getElementById('saida-texto');
+    const copiarmsg = document.getElementById('saida-texto');
     copiarmsg.select();
     document.execCommand("copy");
-    alert("Copiado com Sucesso!");
+    Swal.fire({
+        icon: "success",
+        title: "Copiado",
+        confirmButtonColor: "#3085d6"
+    });
 }
